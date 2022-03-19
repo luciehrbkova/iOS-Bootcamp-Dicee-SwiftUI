a0e26b695c980bbaa9e02ae326f25a531e7f23e1
luciehrbkova <50968340+luciehrbkova@users.noreply.github.com>
2022-03-19 13:28:53 +0000

    setup

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Lucie Hrbkova on 07/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // State is a special wrapper for Swift UI and these element will be generated again when changed
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Button(action: {
+                    // self beacuse we are in closure
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+                    .padding()
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    var n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
